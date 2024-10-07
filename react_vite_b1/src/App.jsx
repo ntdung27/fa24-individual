399480cfa9ab2cd8dfbd09930b9409865578ff5d
ntdung27 <thuydng2004@gmail.com>
2024-10-07 10:10:15 +0700

    delete product without ant design

--- react_vite_b1/src/App.jsx
+++ react_vite_b1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ShowStudent from "./components/ShowStudent";
 
@@ -15,10 +15,76 @@ function App() {
     address: "Quang Ninh",
     isMarried: false,
   };
-  // -------------RETURN
+  // --------------FILTER
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "todo1",
+    },
+    {
+      id: 2,
+      title: "todo2",
+    },
+    {
+      id: 3,
+      title: "todo3",
+    },
+  ]);
+  // -------------PRACTICING
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      namePro: "Product 1",
+      price: 1000,
+      description: "This is the description's product 1",
+      status: true,
+    },
+    {
+      id: 2,
+      namePro: "Product 2",
+      price: 2000,
+      description: "This is the description's product 2",
+      status: true,
+    },
+    {
+      id: 3,
+      namePro: "Product 3",
+      price: 3000,
+      description: "This is the description's product 3",
+      status: false,
+    },
+  ]);
+
+  const removeProducts = (id) => {
+    setProducts(products.filter((items) => items.id !== id));
+    console.log(id);
+  };
+  // --------------------------------INSERT PRODUCTS------------------
+  const [product, setProduct] = useState({});
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!product) return;
+    setProducts([...products, { id: products.length + 1, ...product }]);
+  };
+  // --------------------------------UPDATE PRODUCTS------------------
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+  // -------------------------------useEffect-------------------------
+  const [pro, setPro] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => setPro(data));
+  }, []);
+  // -------------RETURN-----------
   return (
     <>
+      {/* ------------TRUYỀN THAM SỐ CHO 1 COMPONENT */}
       <ShowStudent student={student}></ShowStudent>
+      <hr />
+      <h2>useState</h2>
       <div>
         <button onClick={handleClick}>Change Color</button>
         <div
@@ -27,6 +93,84 @@ function App() {
           Content
         </div>
       </div>
+      <hr />
+      {/* ------------------TODOS--------------------- */}
+      <h2>TODO FORM</h2>
+      <ul>
+        {todos.map((items) => (
+          <li key={items.id}>
+            {items.title}
+            <button
+              onClick={() =>
+                setTodos(todos.filter((todo) => todo.id !== items.id))
+              }
+            >
+              Remove this todo
+            </button>
+          </li>
+        ))}
+      </ul>
+      <hr />
+      {/* -----------------FORM CRUD PRODUCTS------------------- */}
+      <h2>FORM ADD, DELETE PRODUCTS</h2>
+      {/* {JSON.stringify(products)} */}
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="">Tên sản phẩm</label>
+          <input type="text" name="namePro" onChange={onChange} />
+        </div>
+        <div>
+          <label htmlFor="">Giá sản phẩm</label>
+          <input type="number" name="price" onChange={onChange} />
+        </div>
+        <div>
+          <input type="radio" name="status" value={true} onChange={onChange} />{" "}
+          Còn hàng
+          <input
+            type="radio"
+            name="status"
+            value={false}
+            onChange={onChange}
+          />{" "}
+          Hết hàng
+        </div>
+
+        <button>Thêm</button>
+      </form>
+      <table>
+        <thead>
+          <th>#</th>
+          <th>Name Product</th>
+          <th>Price</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          {products.map((items, index) => (
+            <tr key={items.id}>
+              <td>{index + 1}</td>
+              <td>{items.namePro}</td>
+              <td>{items.price}</td>
+              <td>{items.description}</td>
+              <td>{items.status ? "Instock" : "Outstock"}</td>
+              <td>
+                <button onClick={() => removeProducts(items.id)}>
+                  Remove Product
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* ------------------------USE EFFECT------------------ */}
+      <hr />
+      <h2>useEffect</h2>
+      {pro.map((items) => (
+        <div className="" key={items.id}>
+          {items.name}
+        </div>
+      ))}
     </>
   );
 }
--- react_vite_b1/src/components/ShowStudent.jsx
+++ react_vite_b1/src/components/ShowStudent.jsx
@@ -1,8 +1,7 @@
 function ShowStudent({ student }) {
   return (
     <>
-      <h1>Call Component</h1>
-      <h3>Student Information</h3>
+      <h2>Call Component</h2>
       <p>Full Name: {student.fullname}</p>
       <p>Age: {student.age}</p>
       <p>Address: {student.address}</p>
